033ed000ca5630033fc9f54a33deba8b2461a9bb
NathanEshman <nathan.eshman@spartans.ut.edu>
2025-04-29 19:19:45 -0400

    Image Changes
    made Appropriate changes to the image in our bomb phases.

--- bomb_phases.py
+++ bomb_phases.py
@@ -13,9 +13,15 @@ import os
 import sys
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 from PIL import Image
 
 
+=======
+
+
+
+>>>>>>> Stashed changes
 =======
 
 
@@ -68,6 +74,7 @@ class Lcd(tk.Frame):
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         try:
             raw_img = Image.open("/spartans/Final-Project/Ratinpipesmap.jpg")
             raw_img = raw_img.resize((600, 400), Image.ANTIALIAS)
@@ -76,6 +83,9 @@ class Lcd(tk.Frame):
         except Exception as e:
             tk.Label(self, text=f"Image load error: {e}", fg="red", bg="black").grid(row=6, column=0, columnspan=3, pady=20)
 
+=======
+        
+>>>>>>> Stashed changes
 =======
         
 >>>>>>> Stashed changes
