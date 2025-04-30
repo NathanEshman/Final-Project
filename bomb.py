35fb602a3edc1071b4ec3fe10fd69231f5428505
doubled2002 <diaz37021@gmail.com>
2025-04-29 22:29:01 -0400

    updated the def bootup

--- bomb.py
+++ bomb.py
@@ -19,25 +19,21 @@ from bomb_phases import StartScreen
 ###########
 # generates the bootup sequence on the LCD
 def bootup(n=0):
-    # if we're not animating (or we're at the end of the bootup text)
-    if (not ANIMATE or n == len(boot_text)):
-        # if we're not animating, render the entire text at once (and don't process \x00)
-        if (not ANIMATE):
+    if not ANIMATE or n == len(boot_text):
+        if not ANIMATE:
             gui._lscroll["text"] = boot_text.replace("\x00", "")
-        # configure the remaining GUI widgets
         gui.setup()
-        # setup the phase threads, execute them, and check their statuses
-        if (RPi):
+        if RPi:
             setup_phases()
             check_phases()
-    # if we're animating
     else:
-        # add the next character (but don't render \x00 since it specifies a longer pause)
-        if (boot_text[n] != "\x00"):
+        if boot_text[n] != "\x00":
             gui._lscroll["text"] += boot_text[n]
-
-        # scroll the next character after a slight delay (\x00 is a longer delay)
         gui.after(25 if boot_text[n] != "\x00" else 750, bootup, n + 1)
+        # 🟢 Ensure phases are monitored even during animation
+        if n == 0 and RPi:
+            setup_phases()
+            gui.after(100, check_phases)
 
 
 def check_keypad():
@@ -47,22 +43,28 @@ def check_keypad():
 # sets up the phase threads
 def setup_phases():
     global timer, keypad, wires, button, toggles
-
+    
+    # setup the timer thread
     timer = Timer(component_7seg, COUNTDOWN)
+    # bind the 7-segment display to the LCD GUI so that it can be paused/unpaused from the GUI
     gui.setTimer(timer)
-
+    # setup the keypad thread
     keypad = Keypad(component_keypad, keypad_target)
+    # setup the jumper wires thread
     wires = Wires(component_wires, wires_target)
-    button = Button(component_button_state, component_button_RGB, check_all_phases)
+    # setup the pushbutton thread
+    button = Button(component_button_state, component_button_RGB, check_keypad)
+    # bind the pushbutton to the LCD GUI so that its LED can be turned off when we quit
     gui.setButton(button)
+    # setup the toggle switches thread
     toggles = Toggles(component_toggles, toggles_target)
 
+    # start the phase threads
     timer.start()
     keypad.start()
     wires.start()
     button.start()
     toggles.start()
-
 
 # checks the phase threads
 def check_phases():
