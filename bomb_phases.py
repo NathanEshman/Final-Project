86029ef2102fa2d43e1133783828ee32af5b50c2
doubled2002 <diaz37021@gmail.com>
2025-04-30 20:10:27 -0400

    forgot the indent

--- bomb_phases.py
+++ bomb_phases.py
@@ -156,18 +156,18 @@ class Timer(PhaseThread):
 
     # runs the thread
     def run(self):
-    self._running = True
-    while self._running:
-        if not self._paused:
-            self._update()
-            self._component.print(str(self))
-            if self._value == 0:
-                self._running = False
-                break
-            sleep(self._interval)
-            self._value -= 1
-        else:
-            sleep(0.1)
+        self._running = True
+        while self._running:
+            if not self._paused:
+                self._update()
+                self._component.print(str(self))
+                if self._value == 0:
+                    self._running = False
+                    break
+                sleep(self._interval)
+                self._value -= 1
+            else:
+                sleep(0.1)
 
 
     # updates the timer (only internally called)
