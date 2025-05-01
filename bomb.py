9304865d9df93c54e950ea7ea7e649d595f62484
doubled2002 <diaz37021@gmail.com>
2025-04-30 22:43:41 -0400

    again the bootup

--- bomb.py
+++ bomb.py
@@ -18,16 +18,24 @@ import os
 ###########
 # generates the bootup sequence on the LCD
 def bootup(n=0):
-    if not ANIMATE or n == len(boot_text):
-        gui._lscroll["text"] = boot_text.replace("\x00", "")
+    # if we're not animating (or we're at the end of the bootup text)
+    if (not ANIMATE or n == len(boot_text)):
+        # if we're not animating, render the entire text at once (and don't process \x00)
+        if (not ANIMATE):
+            gui._lscroll["text"] = boot_text.replace("\x00", "")
+        # configure the remaining GUI widgets
         gui.setup()
-        gui._lscroll.destroy()
-        if RPi:
+        # setup the phase threads, execute them, and check their statuses
+        if (RPi):
             setup_phases()
             check_phases()
+    # if we're animating
     else:
-        if boot_text[n] != "\x00":
+        # add the next character (but don't render \x00 since it specifies a longer pause)
+        if (boot_text[n] != "\x00"):
             gui._lscroll["text"] += boot_text[n]
+
+        # scroll the next character after a slight delay (\x00 is a longer delay)
         gui.after(25 if boot_text[n] != "\x00" else 750, bootup, n + 1)
 
 def setup_phases():
