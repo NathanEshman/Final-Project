a4443709504c0e7c27299c14c33fb81cb4f2f986
doubled2002 <diaz37021@gmail.com>
2025-05-01 16:21:25 -0400

    forgot to update the bomb.py
    Updated the boot up

--- bomb.py
+++ bomb.py
@@ -14,24 +14,20 @@ from bomb_phases import *
 ###########
 # generates the bootup sequence on the LCD
 def bootup(n=0):
-    # if we're not animating (or we're at the end of the bootup text)
-    if (not ANIMATE or n == len(boot_text)):
-        # if we're not animating, render the entire text at once (and don't process \x00)
-        if (not ANIMATE):
+    if not ANIMATE or n == len(boot_text):
+        if not ANIMATE:
             gui._lscroll["text"] = boot_text.replace("\x00", "")
-        # configure the remaining GUI widgets
-        gui.setup()
-        # setup the phase threads, execute them, and check their statuses
-        if (RPi):
+        # IF riddle is enabled, show riddle screen instead of setup
+        if FIRST_GAME_IS_RIDDLE:
+            gui.showRiddle()
+        else:
+            gui.setup()
+        if RPi:
             setup_phases()
             check_phases()
-    # if we're animating
     else:
-        # add the next character (but don't render \x00 since it specifies a longer pause)
-        if (boot_text[n] != "\x00"):
+        if boot_text[n] != "\x00":
             gui._lscroll["text"] += boot_text[n]
-
-        # scroll the next character after a slight delay (\x00 is a longer delay)
         gui.after(25 if boot_text[n] != "\x00" else 750, bootup, n + 1)
 
 # sets up the phase threads
