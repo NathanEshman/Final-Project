ddd9eb576426b208c5122c520a6099aae61c7b56
doubled2002 <diaz37021@gmail.com>
2025-05-03 22:33:00 -0400

    updated it again

--- bomb.py
+++ bomb.py
@@ -197,14 +197,12 @@ def start_sequence():
 
     def after_boot():
         gui.setup()
-        if FIRST_GAME_IS_RIDDLE or RIDDLE_MODE:
-            gui.showRiddle()  # ✅ Moved here to appear AFTER bootup
         if RPi:
             setup_phases()
             check_phases()
 
-        # Delay just long enough to finish boot text, then show puzzles
-        gui.after(4000, after_boot)
+    # Delay just long enough to finish boot text, then show puzzles
+    gui.after(4000, after_boot)
 
 
 ##########
