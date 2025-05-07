5a419ebfc955fef5c89c64070a6ab729d21d5fbc
doubled2002 <diaz37021@gmail.com>
2025-05-06 22:00:02 -0400

    fix the thread can only be started once error

--- bomb.py
+++ bomb.py
@@ -63,7 +63,6 @@ def setup_phases():
     keypad = Keypad(component_keypad, keypad_target)
     wires = Wires(component_wires, wires_target)
     triangle_puzzle = TrianglePuzzle(6, timer, keypad)  # Use your actual triangle count
-    triangle_puzzle.start()
     button = Button(component_button_state, component_button_RGB, button_target, button_color, timer, triangle_puzzle)
     gui.setButton(button)
     
